dc9953f96cdf8e01a12951d1ec34db591ca6af1c
Vitaliy Sbain <vitaliy-sabin@yandex.ru>
2023-03-14 12:57:50 +0300

    Initial Commit

---
+++ HomeWork - Two Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork - Two Views
+//
+//  Created by Виталий Сабин on 14.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
